c0867c934699468cba2d4116a195537d43ff58bf
Theodoros <tchara05@cs.ucy.ac.cy>
2015-04-08 19:13:32 +0300

    Database Connection , JSON parser

---
+++ ArkhamAsylumSystem/src/ArkhamAsylumSystem/Reports.java
@@ -0,0 +1,43 @@
+package ArkhamAsylumSystem;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import extras.DatabaseConnection;
+import extras.JSON;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+
+
+@Path("/report/")
+
+public class Reports {
+
+	@GET
+	@Path("/all_patients/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getPatient() throws Exception{
+		
+		DatabaseConnection database = new DatabaseConnection();
+		
+		if (database.getStatement() == null){
+			return "Gamiseta";
+		}
+		
+		ResultSet rs= database.getStatement().executeQuery("SELECT * FROM PATIENT");
+		
+		String result = JSON.parseJSON(rs);
+		
+		return  result;
+		
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ ArkhamAsylumSystem/src/extras/DatabaseConnection.java
@@ -0,0 +1,48 @@
+package extras;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DatabaseConnection {
+	
+	private Connection conn;
+	private Statement statement;
+
+	public DatabaseConnection(){
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager
+					.getConnection("jdbc:mysql://localhost:3306/EPL362","root", "");
+			statement = conn.createStatement();
+			
+			if (conn == null){
+				System.out.println("adsasa");
+			}	
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public Statement getStatement(){
+		return statement;
+	}
+	
+
+	public void CloseConnection(){
+		try {
+			statement.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+
+}
