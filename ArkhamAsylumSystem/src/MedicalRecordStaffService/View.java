f983ed19055b98f3f0426f58c7d69dd9579042dc
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2015-04-16 19:09:08 +0300

    view personel

--- ArkhamAsylumSystem/src/MedicalRecordStaffService/View.java
+++ ArkhamAsylumSystem/src/MedicalRecordStaffService/View.java
@@ -2,7 +2,7 @@ package MedicalRecordStaffService;
 
 import java.sql.ResultSet;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -13,7 +13,7 @@ import extras.JSON;
 @Path("/medical_record_service/")
 public class View {
 
-	@GET
+	@POST
 	@Path("/view_patient_requests/")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String getRequests() throws Exception{
@@ -21,10 +21,22 @@ public class View {
 		if (database.getStatement() == null){
 			return "No requests have been found";
 		}
-		ResultSet rs= database.getStatement().executeQuery("SELECT * FROM REQUEST");
+		ResultSet rs= database.getStatement().executeQuery("SELECT REQUEST.id AS `number`,REQUEST.date,REQUEST.content,PATIENT.firstname,PATIENT.lastname,PATIENT.id FROM REQUEST,PATIENT where REQUEST.patient=PATIENT.id;");
 		String result = JSON.parseJSON(rs);
 		return  result;
 	}
 	
+	@POST
+	@Path("/view_personel_info/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getPersonel() throws Exception{
+		DatabaseConnection database = new DatabaseConnection();
+		if (database.getStatement() == null){
+			return "No requests have been found";
+		}
+		ResultSet rs= database.getStatement().executeQuery("SELECT * FROM USER;");
+		String result = JSON.parseJSON(rs);
+		return  result;
+	}
 	
 }
