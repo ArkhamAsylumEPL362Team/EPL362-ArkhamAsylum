b51fa850dbf1b992b23ad0348e39ad3d9d0b9496
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2015-04-16 18:49:11 +0300

    9: Fix "The requested resource is not available."
    Task-Url:
    https://github.com/ArkhamAsylumEPL362Team/EPL362-ArkhamAsylum/issues/issue/9

--- ArkhamAsylumSystem/src/MedicalRecordStaffService/View.java
+++ ArkhamAsylumSystem/src/MedicalRecordStaffService/View.java
@@ -1,5 +1,30 @@
 package MedicalRecordStaffService;
 
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import extras.DatabaseConnection;
+import extras.JSON;
+
+@Path("/medical_record_service/")
 public class View {
 
+	@GET
+	@Path("/view_patient_requests/")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getRequests() throws Exception{
+		DatabaseConnection database = new DatabaseConnection();
+		if (database.getStatement() == null){
+			return "No requests have been found";
+		}
+		ResultSet rs= database.getStatement().executeQuery("SELECT * FROM REQUEST");
+		String result = JSON.parseJSON(rs);
+		return  result;
+	}
+	
+	
 }
