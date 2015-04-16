86b4fac1861a9bec704abbde8f25b2556b0cfe5e
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2015-04-16 09:24:21 +0300

    database connector constants theodore! CONSTANTS!! I've been looking into the error for hours!

--- ArkhamAsylumSystem/src/extras/DatabaseConnection.java
+++ ArkhamAsylumSystem/src/extras/DatabaseConnection.java
@@ -9,13 +9,17 @@ public class DatabaseConnection {
 	
 	private Connection conn;
 	private Statement statement;
-
+	private static final String HOST="localhost:3306";
+	private static final String USER="root";
+	private static final String PASSWORD="";
+	private static final String DATABASE="arkham_db";
+	
 	public DatabaseConnection(){
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/EPL362","root", "");
+					.getConnection("jdbc:mysql://"+HOST+"/"+DATABASE,USER, PASSWORD);
 			statement = conn.createStatement();
 			
 			if (conn == null){
